faf0de4d5da57d74dc179b4c8aac788426ca0edc
aleksmn <al-mn@yandex.ru>
2025-03-01 11:41:36 +0300

    add navbar to App page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,9 +14,25 @@ function App() {
 
   return (
     <div className="App">
+      <nav>
       <h1>Welcome to MERN base app</h1>
+
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      </nav>
+
       <h2>{message}</h2>
       <img src="/images/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg" alt="" />
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos dignissimos hic eos a similique fugit esse provident, et exercitationem, quidem, velit error! Molestias, voluptate rerum adipisci repudiandae suscipit quia cupiditate!</p>
+      <p>Maiores commodi tenetur hic voluptas dolore culpa quisquam, sapiente cum voluptatem nisi ullam quos rerum ratione qui quas pariatur quis explicabo, accusantium odio! Fugit velit obcaecati error excepturi corporis dolorem.</p>
+      <p>Aliquam enim voluptate quod incidunt. Accusamus natus harum, eius excepturi consequatur quam ipsa totam possimus iure, odio, aliquid quaerat odit dignissimos! Ipsum quas nemo vero voluptas tenetur saepe in optio?</p>
+      <p>Dignissimos odit harum esse quas. Et itaque beatae provident molestias deleniti voluptatibus iure animi quos modi alias, inventore veritatis quasi molestiae, nostrum sapiente adipisci eveniet excepturi fugiat optio non facere!</p>
+      <p>Id nihil facilis fugiat quaerat praesentium. Corrupti ut, id aut cupiditate eveniet assumenda qui enim ratione numquam illo iste tenetur error atque asperiores accusamus inventore excepturi! Praesentium quae sunt id.</p>
     </div>
   );
 }
